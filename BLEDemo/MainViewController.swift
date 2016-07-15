e951421aa302403ef6a120eb5b046ee257579909
Rick Smith <rick.smith@analogfolk.com>
2016-07-15 16:46:07 +0100

    initial commit

---
+++ BLEDemo/MainViewController.swift
@@ -0,0 +1,205 @@
+//
+//  MainViewController.swift
+//  CompassCompanion
+//
+//  Created by Rick Smith on 04/07/2016.
+//  Copyright © 2016 Rick Smith. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class MainViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    var manager:CBCentralManager? = nil
+    var mainPeripheral:CBPeripheral? = nil
+    var mainCharacteristic:CBCharacteristic? = nil
+    
+    let BLEService = "DFB0"
+    let BLECharacteristic = "DFB1"
+    
+    @IBOutlet weak var recievedMessageText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        manager = CBCentralManager(delegate: self, queue: nil);
+        
+        customiseNavigationBar()
+    }
+    
+    func customiseNavigationBar () {
+        
+        self.navigationItem.rightBarButtonItem = nil
+        
+        let rightButton = UIButton()
+        
+        if (mainPeripheral == nil) {
+            rightButton.setTitle("Scan", for: [])
+            rightButton.setTitleColor(UIColor.blue(), for: [])
+            rightButton.frame = CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: 60, height: 30))
+            rightButton.addTarget(self, action: #selector(self.scanButtonPressed), for: .touchUpInside)
+        } else {
+            rightButton.setTitle("Disconnect", for: [])
+            rightButton.setTitleColor(UIColor.blue(), for: [])
+            rightButton.frame = CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: 100, height: 30))
+            rightButton.addTarget(self, action: #selector(self.disconnectButtonPressed), for: .touchUpInside)
+        }
+        
+        let rightBarButton = UIBarButtonItem()
+        rightBarButton.customView = rightButton
+        self.navigationItem.rightBarButtonItem = rightBarButton
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if (segue.identifier == "scan-segue") {
+            let scanController : ScanTableViewController = segue.destinationViewController as! ScanTableViewController
+            
+            //set the manager's delegate to the scan view so it can call relevant connection methods
+            manager?.delegate = scanController
+            scanController.manager = manager
+            scanController.parentView = self
+        }
+        
+    }
+    
+    // MARK: Button Methods
+    func scanButtonPressed() {
+        performSegue(withIdentifier: "scan-segue", sender: nil)
+    }
+    
+    func disconnectButtonPressed() {
+        //this will call didDisconnectPeripheral, but if any other apps are using the device it will not immediately disconnect
+        manager?.cancelPeripheralConnection(mainPeripheral!)
+    }
+    
+    @IBAction func sendButtonPressed(_ sender: AnyObject) {
+        let helloWorld = "Hello World!"
+        let dataToSend = helloWorld.data(using: String.Encoding.utf8)
+        
+        if (mainPeripheral != nil) {
+            mainPeripheral?.writeValue(dataToSend!, for: mainCharacteristic!, type: CBCharacteristicWriteType.withoutResponse)
+        } else {
+            print("haven't discovered device yet")
+        }
+    }
+    
+    // MARK: - CBCentralManagerDelegate Methods
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        mainPeripheral = nil
+        customiseNavigationBar()
+        print("Disconnected" + peripheral.name!)
+    }
+    
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        print(central.state)
+    }
+    
+    // MARK: CBPeripheralDelegate Methods
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: NSError?) {
+        
+        for service in peripheral.services! {
+            
+            print("Service found with UUID: " + service.uuid.uuidString)
+            
+            //device information service
+            if (service.uuid.uuidString == "180A") {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+            
+            //GAP (Generic Access Profile) for Device Name
+            // This replaces the deprecated CBUUIDGenericAccessProfileString
+            if (service.uuid.uuidString == "1800") {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+            
+            //Bluno Service
+            if (service.uuid.uuidString == BLEService) {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+            
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: NSError?) {
+
+        //get device name
+        if (service.uuid.uuidString == "1800") {
+            
+            for characteristic in service.characteristics! {
+                
+                if (characteristic.uuid.uuidString == "2A00") {
+                    peripheral.readValue(for: characteristic)
+                    print("Found Device Name Characteristic")
+                }
+                
+            }
+            
+        }
+        
+        if (service.uuid.uuidString == "180A") {
+            
+            for characteristic in service.characteristics! {
+                
+                if (characteristic.uuid.uuidString == "2A29") {
+                    peripheral.readValue(for: characteristic)
+                    print("Found a Device Manufacturer Name Characteristic")
+                } else if (characteristic.uuid.uuidString == "2A23") {
+                    peripheral.readValue(for: characteristic)
+                    print("Found System ID")
+                }
+                
+            }
+            
+        }
+        
+        if (service.uuid.uuidString == BLEService) {
+            
+            for characteristic in service.characteristics! {
+                
+                if (characteristic.uuid.uuidString == BLECharacteristic) {
+                    //we'll save the reference, we need it to write data
+                    mainCharacteristic = characteristic
+                    
+                    //Set Notify is useful to read incoming data async
+                    peripheral.setNotifyValue(true, for: characteristic)
+                    print("Found Bluno Data Characteristic")
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: NSError?) {
+        
+        
+        if (characteristic.uuid.uuidString == "2A00") {
+            //value for device name recieved
+            let deviceName = characteristic.value
+            print(deviceName)
+        } else if (characteristic.uuid.uuidString == "2A29") {
+            //value for manufacturer name recieved
+            let manufacturerName = characteristic.value
+            print(manufacturerName)
+        } else if (characteristic.uuid.uuidString == "2A23") {
+            //value for system ID recieved
+            let systemID = characteristic.value
+            print(systemID)
+        } else if (characteristic.uuid.uuidString == BLECharacteristic) {
+            //data recieved
+            if(characteristic.value != nil) {
+                let stringValue = String(data: characteristic.value!, encoding: String.Encoding.utf8)!
+            
+                recievedMessageText.text = stringValue
+            }
+        }
+        
+        
+    }
+    
+}
+
---
+++ BLEDemo/ScanTableViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ScanTableViewController.swift
+//  BLEDemo
+//
+//  Created by Rick Smith on 13/07/2016.
+//  Copyright © 2016 Rick Smith. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class ScanTableViewController: UITableViewController, CBCentralManagerDelegate {
+    
+    var peripherals:[CBPeripheral] = []
+    var manager:CBCentralManager? = nil
+    var parentView:MainViewController? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()    
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        scanBLEDevices()
+    }    
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return peripherals.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "scanTableCell", for: indexPath)
+        let peripheral = peripherals[indexPath.row]
+        cell.textLabel?.text = peripheral.name
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let peripheral = peripherals[indexPath.row]
+        
+        manager?.connect(peripheral, options: nil)
+    }
+    
+    // MARK: BLE Scanning
+    func scanBLEDevices() {
+        manager?.scanForPeripherals(withServices: [CBUUID.init(string: parentView!.BLEService)], options: nil)
+        
+        //stop scanning after 3 seconds
+        DispatchQueue.main.after(when: .now() + 3.0) {
+            self.stopScanForBLEDevices()
+        }
+    }
+    
+    func stopScanForBLEDevices() {
+        manager?.stopScan()
+    }
+    
+    // MARK: - CBCentralManagerDelegate Methods
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : AnyObject], rssi RSSI: NSNumber) {
+        
+        if(!peripherals.contains(peripheral)) {
+            peripherals.append(peripheral)
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        print(central.state)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        
+        //pass reference to connected peripheral to parent view
+        parentView?.mainPeripheral = peripheral
+        peripheral.delegate = parentView
+        peripheral.discoverServices(nil)
+        
+        //set the manager's delegate view to parent so it can call relevant disconnect methods
+        manager?.delegate = parentView
+        parentView?.customiseNavigationBar()
+        
+        if let navController = self.navigationController {
+            navController.popViewController(animated: true)
+        }
+        
+        print("Connected to " +  peripheral.name!)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: NSError?) {
+        print(error)
+    }
+    
+}
